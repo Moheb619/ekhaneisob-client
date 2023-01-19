f16c7d12144c6bb5a0014d01c543c8ad68f1b1b2
Moheb619 <6moheb19@gmail.com>
2023-01-20 01:46:34 +0600

    sidebar toggle added

--- src/app/common-components/sidebar/sidebar.component.ts
+++ src/app/common-components/sidebar/sidebar.component.ts
@@ -9,7 +9,7 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent implements OnInit {
-  openSidebar: string = 'electronics';
+  openSidebar: string;
   subscription: Subscription;
   CATEGORY_DATA: CategoryModel[] = [];
   openedSidebarFunction(activeSidebarName: string) {
@@ -21,6 +21,7 @@ export class SidebarComponent implements OnInit {
       .getCategories()
       .subscribe((data: CategoryModel[]) => {
         this.CATEGORY_DATA = data;
+        this.openSidebar = data[0].name;
       });
   }
 }
